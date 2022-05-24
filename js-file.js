aef39f6a5cf5c0baa0fcba97dc82cd489c7e361a
NateTewolde <tewolde.nate@gmail.com>
2022-05-24 14:33:31 -0700

    Create 16x16 grid

--- js-file.js
+++ js-file.js
@@ -1,7 +1,17 @@
+const container = document.querySelector("#container");
+const grid = [];
+
+createGrid();
+
 function createGrid() {
-  for (let i = 1; i <= 16; i++) {
-    for (let j = 1; j <= 16; j++) {
+  for (let i = 0; i < 16; i++) {
+    grid[i] = document.createElement("div");
+    grid[i].classList.add("grid");
+    container.appendChild(grid[i]);
+    for (let j = 0; j < 16; j++) {
       const cell = document.createElement("div");
+      cell.classList.add("cell");
+      grid[i].appendChild(cell);
     }
   }
 }
