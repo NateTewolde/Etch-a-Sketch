b28e0dc0b53d7262ae541a97d4a33e47be7af761
NateTewolde <tewolde.nate@gmail.com>
2022-05-24 16:12:03 -0700

    Allow the user to create a grid size of their choosing by pressing Choose Dimensions  button and entering a numeric value

--- js-file.js
+++ js-file.js
@@ -1,21 +1,26 @@
 const container = document.querySelector("#container");
 const grid = [];
 
-createGrid();
-userGrid();
+createGrid(16);
+userGridSize();
 hoverTrail();
 
-function userGrid() {
-  const btn = document.createElement("button");
-  btn.classList.add("btn");
+function userGridSize() {
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    let userSize = prompt("How many squares per side would you like?");
+    removeAllChildNodes(container);
+    createGrid(userSize);
+    hoverTrail();
+  });
 }
 
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
+function createGrid(squareAmount) {
+  for (let i = 0; i < squareAmount; i++) {
     grid[i] = document.createElement("div");
     grid[i].classList.add("grid");
     container.appendChild(grid[i]);
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < squareAmount; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       grid[i].appendChild(cell);
@@ -30,4 +35,10 @@ function hoverTrail() {
       cell.setAttribute("style", "background: blue;");
     });
   });
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
